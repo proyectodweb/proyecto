690bc8f4dd795a990a55930ff9e6373452c01ba1
ProyectoDweb <karinasierrairiarte@gmail.com>
2021-08-17 12:54:39 -0300

    alertas de usuario y contraseña

--- js/login.js
+++ js/login.js
@@ -1,6 +1,23 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function verificar(){
+let dato = document.getElementById("user");
+let usuario = {};
+let pass = document.getElementById("pwd");
 
+if ((dato.value.trim() === '')) || (dato.value.trim() ===''))
+{
+    alert("Ingresa tu nombre y contraseña para comenzar!");
+}else {
+    location.href ="index.html";
+    usuario.nombre =dato.value;
+    usuario.estado ="Conectado"
+
+    localStorage.setItem('usuario',JSON.stringify(usuario)),
+}
 });
+function desconectar (){
+    localStorage.clear();
+    location.href ="index.html"
+}
