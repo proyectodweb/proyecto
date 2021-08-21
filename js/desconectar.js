f09d8bd13f17d9677f26cf0d0f65cc0a5f8b0821
ProyectoDweb <karinasierrairiarte@gmail.com>
2021-08-21 18:46:19 -0300

    Boton desconectar

---
+++ js/desconectar.js
@@ -0,0 +1,20 @@
+function desconectar() {
+    localStorage.clear();
+    signOut()
+    location.href= "login.html"
+}
+// csi deshabilito sign out --con local host al desconectar me vuelve al index. de la otra forma no
+// si uso signout y sin local host el boton desconectar no anda, y con el bot de google tampoco
+
+function signOut() {
+      var auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(function () {
+        location.href= "login.html"
+      });
+      
+    }
+    function onLoad() {
+        gapi.load('auth2', function() {
+            gapi.auth2.init();
+        });
+    }
