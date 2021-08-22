f8b02b5979d7aa6d3fec95fdbf8ac1bad56e56a7
ProyectoDweb <karinasierrairiarte@gmail.com>
2021-08-22 17:08:33 -0300

    rename

--- js/products.js
+++ js/products.js
@@ -2,14 +2,14 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-getJSONData(PRODUCTS_URL).then(function(resultObj) {
-    if (resultObj.status === "ok") {
-    categoriesArray = resultObj.data;
-    showCategoriesList(categoriesArray);    
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+        categoriesArray = resultObj.data;
+        showCategoriesList(categoriesArray);    
+        }
     }
-}
-);
-});
+    );
+    });
 
 var categoriesArray = [];
 
@@ -22,13 +22,13 @@ function showCategoriesList(array){
         let product = array[i];
         htmlContentToAppend +=`
         <div class="list-group-item list-group-item-action">
-            <div class="row">
+      <div class="row">
          
                 <div class="col-3">
-                <img src="` + product.imgSrc + `"alt="` + product.description + `" class="img-thumbnail">
-                </div>     
+                         <img src="` + product.imgSrc + `"alt="` + product.description + `" class="img-thumbnail">
+                        </div>     
                  </div>
-                    <div class="col">
+                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1"><b>` + product.name +` </b> </h4>
                         <small class="text-muted">` + product.soldCount +` Vendidos</small>
@@ -36,7 +36,7 @@ function showCategoriesList(array){
                     <p class="mb-1">` + product.description +` </p>
                          <div class="w-100"><b>` + product.currency + "  " + product.cost +` </b>
                          </div>
-                </div>
+                  </div>
             </div>
         </div>
         `
