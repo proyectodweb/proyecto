a0f8855d83d047a9ca5f12e5d8baf0dffb444495
ProyectoDweb <karinasierrairiarte@gmail.com>
2021-10-18 00:23:49 -0300

    carrito 2 prod

--- js/cart.js
+++ js/cart.js
@@ -7,25 +7,32 @@ function showCart(array) {
     for (let i = 0; i < array.length; i++) {
 
         let compras = array[i];
-
+        cantP = compras.count;
         htmlContentToAppend += `
         <hr>
         <div class="row">
-                        <div class="col-3 carri">
+                        <div class="col-4 carri">
                             <h6>`+ compras.name + `</h6>
-                           <img src="` + compras.src + `" class="img-thumbnail">    
+                            <button type="button" id="trash"> <i class="fa fa-trash-o fa-2x" aria-hidden="true"></i></button>
+                            <img src="` + compras.src + `" class="img-thumbnail imgcarrito" id="img-carrito">
                         </div>
                                    
-                        <div class="col-2 carri"><b>` + compras.unitCost + ` </b> </div>
                         <div class="col-2 carri"><b>` + compras.currency + ` </b> </div>
-                        <div class="col-2 carri"><b>` + compras.count + ` </b> </div>
-                        <div id="subT" class="col-2 carri"><b>` + + ` </b> </div>
+                        <div class="col-2 carri"><b>` + compras.unitCost + ` </b> </div>
+                        <div class="col-2 carri"><b>
+                        <input type="number" value="` + compras.count + `" min="0" id='cant' onchange='sumar()' size="5"> </b> </div>
+                        
+                        <div id="subT" class="col-2 carri"><b>` + (compras.unitCost * cantP) + ` </b> </div>
+                        
           </div>
-         
+       
           `
         document.getElementById("mostrarcarrito").innerHTML = htmlContentToAppend;
     };
 }
+
+//SUBTOTAL = MULTIPLICAR LAS VARIABLES DENTRO DEL MISMO FOR! HERMOSO!
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -43,9 +50,10 @@ document.addEventListener("DOMContentLoaded", function (e) {
     });
 });
 
-function subTotal() {
-    
-    let subtotal = compras.unitCost * compras.count
 
-    
+
+function sumar() {
+
+    let cantprod = document.getElementById("cant").value;
+    let precprod = document.getElementById("")
 }
