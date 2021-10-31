2612b4fdca035d2e182a8d5959d076f33c4cf601
ProyectoDweb <karinasierrairiarte@gmail.com>
2021-10-31 17:38:14 -0300

    local storage

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,25 +1,44 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-//document.addEventListener("DOMContentLoaded", function (e) {
 
-//});
+  function previewFile() {
 
-let perfiles = [];
-let perfil = {};
-    function guardarPerfil() { 
-      // let dato = document.getElementById("nombre");
+  let preview = document.getElementById("foto");
+  let file = document.querySelector("input[type=file]").files[0];
+  let reader = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+    document.getElementById("contenido").innerHTML = reader.result;
+  }
+
+if (file) {
+  reader.readAsDataURL(file);
+} else { 
+  preview.src = "img/avatar.png";
+}
+}  
+
+
+var perfiles = [];
+var perfil = {};
+
+  function guardarPerfil() { 
+      
     let perfil = {};
-
+    let preview = document.getElementById("foto");
+    
     perfil.nombre = document.getElementById("fname").value;
     perfil.apellido = document.getElementById("lname").value;
     perfil.email = document.getElementById("email").value;
     perfil.cel = document.getElementById("cel").value;
     perfil.edad = document.getElementById("age").value;
-       // perfil.imagen = document.getElementById("perfiimg")
+    perfil.imagen = preview.src
     
 
-    localStorage.setItem("perfiles", JSON.stringify(perfil));
+    localStorage.setItem("users", JSON.stringify(perfil));
+    alert("perfil guardado")
     
 //vaciar campos
 //    document.getElementById("fname").value = "";
@@ -31,10 +50,38 @@ let perfil = {};
 
     perfil.push(perfiles)
 
+
+    document.addEventListener("DOMContentLoaded", function (e) {
+      let preview = document.getElementById("foto");
+      let perfil = JSON.parse(localStorage.getItem("usuario"));
+
+      if (perfil != null){
+
+        document.getElementById("lname").value = perfil.nombre
+        document.getElementById("age").value = perfil.edad;
+        document.getElementById("foto").value = perfil.imagen
+      }else{
+        preview.src = "img/avatar.png"
+      }
+    });
+
     /*function modificarPerfil() {
-        alert("modifica tus datos y guardalos nuevamente :)");
+      //  alert("modifica tus datos y guardalos nuevamente :)");
 
-        //let perfil = JSON.parse(localStorage.getItem("perfiles"));
+        let perfil = JSON.parse(localStorage.getItem("perfil"));
 
-        document.getElementById("fname").innerHTML = perfil.nombre;
-    }*/
+        perfil.nombre += document.getElementById("fname").innerHTML;
+    }
+    
+    document.querySelector("#myFileInput").addEventListener("change", function () {
+  const reader = new FileReader();
+
+  reader.addEventListener("load", () => {
+    localStorage.setItem("recent-image", reader.result);
+  })
+
+  reader.readAsDataURL(this.files[0]);
+}
+)
+
+*/
